7ff694c3720269b19593960ff7af70059d253a65
magdakajszczak <madzia.kajszczak@gmail.com>
2022-01-20 18:29:44 +0100

    Add basic validations

--- Converter/ViewController.swift
+++ Converter/ViewController.swift
@@ -6,15 +6,15 @@
 //
 
 import UIKit
+
 struct Price: Codable {
     let EUR_USD: Double
-    
 }
 
 class ViewController: UIViewController {
     
     
-    @IBOutlet weak var InputValue: UITextField!
+    @IBOutlet weak var inputValue: UITextField!
     @IBOutlet weak var outputValue: UILabel!
 
     override func viewDidLoad() {
@@ -23,19 +23,28 @@ class ViewController: UIViewController {
     
     
     @IBAction func converterButton(_ sender: UIButton) {
+        
         let apiKey = "YOUR API KEY"
-        let currency = Double(self.InputValue.text!)!
+       
         let url = URL(string: "https://free.currconv.com/api/v7/convert?q=EUR_usd&compact=ultra&apiKey="+apiKey)!
         
+        let alert = UIAlertController(title: "Błąd", message: "Wartość nie może być pusta!", preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        guard let currency = Double((self.inputValue.text!)) else  {
+            return self.present(alert, animated: true, completion: nil)
+        }
+       
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
             if let data = data {
                 do {
-                    let rest = try JSONDecoder().decode(Price.self, from: data)
-                    let saveValue = rest.EUR_USD * currency
-                    let convertToString =  String(saveValue)
+                    let jsonDecode = try JSONDecoder().decode(Price.self, from: data)
+                    let saveValue = jsonDecode.EUR_USD * currency
+                    let convertToString = String(saveValue)
                     
                     DispatchQueue.main.async {
-                        self.outputValue.text = "Kwota w dolarach: " + convertToString
+                        self.outputValue.text = "Kwota w USD: " + convertToString
                     }
                     
                 }
